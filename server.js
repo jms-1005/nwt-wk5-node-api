6815aecaeaf2b59e1f708efb2f435dca8d7f0651
John Manoah <john@vanarts.com>
2023-07-28 10:24:33 -0700

    added env and mysql2

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 //Express is a library/dependency/module 
 // Express is for node, just like what Bootstrap is for CSS
 import express from 'express';
+import mysql from 'mysql2';
+import 'dotenv/config';
 
 // Create an object 
 const server = express();
@@ -12,6 +14,22 @@ server.listen(port, function(){
     console.log('Server started and running on port no', port);
 });
 
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: 'Store'
+})
+
+db.connect(function(error){
+    if(error){
+        console.log('Connection to SQL failed', error);
+    }
+   else{
+        console.log('Successfully connected to MySQL');
+   }
+})
 
 // Lets design the APIs
 
@@ -28,18 +46,28 @@ server.get('/greetings', function(req, res){
 });
 
 server.get('/products', function(req, res){
-    res.json([
-        {
-            id: 1,
-            name: 'iPhone',
-            price: 999
-        },
-        {
-            id: 2,
-            name: 'Samsung',
-            price: 1199
+    let SQLQuery = "CALL `getAllProducts`()";
+    db.query(SQLQuery, function(error, data){
+        if(error){
+            res.json({ error_message: error });
         }
-    ]);
+        else{
+            res.json({ products: data});
+        }
+    })
+
+    // res.json([
+    //     {
+    //         id: 1,
+    //         name: 'iPhone',
+    //         price: 999
+    //     },
+    //     {
+    //         id: 2,
+    //         name: 'Samsung',
+    //         price: 1199
+    //     }
+    // ]);
 })
 
 server.get('/contact', function(req, res){
@@ -57,3 +85,17 @@ server.post('/send', function(req, res){
     //console.log(req.body);
     res.json({ message: "received your message - " + req.body.greetings })
 })
+
+server.post('/addnewproduct', function(req, res){
+    let SQLQuery = "CALL `addNewProduct`(?, ?)";
+    db.query(SQLQuery, [req.body.name, req.body.price], function(error, data){
+        if(error){
+            res.json({ error_message: error })
+        }
+        else{
+            res.json({ message: "Product added successfully" });
+        }
+    })
+})
+
+// server.put
